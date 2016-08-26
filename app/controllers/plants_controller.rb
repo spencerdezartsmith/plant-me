f4ef8646d0772cda856bc5a8bcb9e1056a061499
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-08-25 19:24:57 -0700

    Add search logic to plant controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -4,15 +4,17 @@ class PlantsController < ApplicationController
   end
 
   def search
-    case size
-      when params['plant']['size'] == 1
-        1...2
-      when params['plant']['size'] == 2
-        2...4
-      when params['plant']['size'] == 3
-        > 3
-      end
-    else
-    @plants = Plant.where(light_requirement: params['plants']['light'], water_requirement: params['plants']['water'], max_height: size)
+    @plants = Plant.where(light_requirement: params['plant']['light'], water_requirement: params['plant']['water'], max_height: plant_size(params['plant']['size'].to_i))
   end
+
+  private
+    def plant_size(params)
+      if params == 1
+        return (1..2)
+      elsif params == 2
+          return 2..4
+      elsif params == 3
+          return 4..20
+      end
+    end
 end
